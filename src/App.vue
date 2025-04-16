51e99af218a870a194981b1fda16ae6179e02ead
Arthur <arthur.rob.dev@gmail.com>
2025-04-16 15:45:52 +0200

    fix(tailwind): remove postcss and use tailwind vite config

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template class="bg-slate-200">
-    <Menu />
     <Layout>
         <router-view></router-view>
     </Layout>
-    <ActionBars />
 </template>
 <script lang="ts" setup>
 import '@/index.css'
 import { useIndexStore } from '@/store/index'
-import Menu from '@/components/menu.vue'
-import ActionBars from '@/components/actions.vue'
 import Layout from '@/views/layout.vue'
 const indexStore = useIndexStore()
 indexStore.syncPlotter()
