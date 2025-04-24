de876c83af25a9920c130b910616100e4cc586a7
Arthur <arthur.rob.dev@gmail.com>
2025-04-24 09:45:18 +0200

    core(store): store and api fix

--- src/App.vue
+++ src/App.vue
@@ -13,22 +13,14 @@ indexStore.syncPlotter()
 </script>
 <style lang="scss" scoped>
 .app-index {
-    background-image:
-        repeating-linear-gradient(
-            315deg,
-            var(--color-gray-700) 0,
-            var(--color-gray-700) 1px,
-            transparent 0,
-            transparent 50%
-        ),
-        repeating-linear-gradient(
-            45deg,
-            var(--color-gray-700) 0,
-            var(--color-gray-700) 1px,
-            transparent 0,
-            transparent 50%
-        );
+    background-image: repeating-linear-gradient(
+        315deg,
+        var(--color-gray-700) 0,
+        var(--color-gray-700) 1px,
+        transparent 0,
+        transparent 50%
+    );
     background-attachment: fixed;
-    background-size: 30px 30px;
+    background-size: 10px 10px;
 }
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,12 @@
+<template>
+    <EditorLayout></EditorLayout>
+</template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import { useIndexStore } from '@/store/index'
 import Circle from '@/lib/geometry/Circle'
 
 const stylo = useIndexStore().stylo
-const plotterList = ref<string[]>()
 const c1 = new Circle(50, 40, 10)
 const c2 = new Circle(20, 20, 10)
 stylo.add(c1)
@@ -13,6 +15,5 @@ stylo.add(c2)
 onMounted(async () => {
     stylo.init('#stylo', { renderSize: 2 })
     stylo.render()
-    plotterList.value = await stylo.listPlotter()
 })
 </script>
